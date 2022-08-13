4bc37599a221d398d767d32d32d5c0dba1994588
Daksh-raj <medhavisharma300@gmail.com>
2022-08-14 00:26:42 +0530

    finally ready with animation

--- fetch.js
+++ fetch.js
@@ -8,7 +8,7 @@ for (let i = 0; i < 5; i++) {
   drop_down_container[i].addEventListener("click", (e) => {
     input_value = e.target.innerText;
     // console.log(input_value);
-   input_value= input_value.toLowerCase();
+    input_value = input_value.toLowerCase();
     if (input_value == "") {
       alert("Not a valid input");
     } else {
@@ -23,63 +23,62 @@ for (let i = 0; i < 5; i++) {
           // console.log(input_value);
           if (input_value.includes("monday")) {
             // console.log(myObj.time_table.monday["11.00AM-12.00AM"]);
-            let final = "";
+            let final="";
             for (let key in myObj.time_table.monday) {
-              let container_temp = document.createElement("div");
-              container_temp.className="container_temp";
-              container_temp = "";
-              container_temp += `<h1 style="color:"red">${key}
-              <h2 style="color:blue">${myObj.time_table.monday[key]}</h2><br>`;
-              console.log(container_temp);
-              final += container_temp;
+              final += `<div class="card animate__pulse">
+              <div class="card-body">
+                  <h5 class="card-title">${key}</h5>
+                  <p class="card-text">${myObj.time_table.monday[key]}</p>
+              </div>
+          </div>`;
             }
             container_push.innerHTML = final;
           } else if (input_value.includes("tuesday")) {
             console.log(myObj.time_table.tuesday["11.00AM-12.00AM"]);
             let final = " ";
             for (let key in myObj.time_table.tuesday) {
-              let container_temp = document.createElement("div");
-              container_temp = "";
-              container_temp += `<h1 style="color:"red">${key}
-              <h2 style="color:blue">${myObj.time_table.tuesday[key]}</h2><br>`;
-              console.log(container_temp);
-              final += container_temp;
+              final += `<div class="card animate__pulse">
+              <div class="card-body">
+                  <h5 class="card-title">${key}</h5>
+                  <p class="card-text">${myObj.time_table.tuesday[key]}</p>
+              </div>
+          </div>`
             }
             container_push.innerHTML = final;
           } else if (input_value.includes("wednesday")) {
             let final = " ";
             console.log(myObj.time_table.wednesday["11.00AM-12.00AM"]);
             for (let key in myObj.time_table.wednesday) {
-              let container_temp = document.createElement("div");
-              container_temp = "";
-              container_temp += `<h1 style="color:"red">${key}
-              <h2 style="color:blue">${myObj.time_table.wednesday[key]}</h2><br>`;
-              console.log(container_temp);
-              final += container_temp;
+              final += `<div class="card animate__pulse">
+              <div class="card-body">
+                  <h5 class="card-title">${key}</h5>
+                  <p class="card-text">${myObj.time_table.wednesday[key]}</p>
+              </div>
+          </div>`
             }
             container_push.innerHTML = final;
           } else if (input_value.includes("thursday")) {
             let final = "";
             console.log(myObj.time_table.thursday["11.00AM-12.00AM"]);
             for (let key in myObj.time_table.thursday) {
-              let container_temp = document.createElement("div");
-              container_temp = "";
-              container_temp += `<h1 style="color:"red">${key}
-              <h2 style="color:blue">${myObj.time_table.thursday[key]}</h2><br>`;
-              console.log(container_temp);
-              final += container_temp;
+              final += `<div class="card animate__pulse">
+              <div class="card-body">
+                  <h5 class="card-title">${key}</h5>
+                  <p class="card-text">${myObj.time_table.thursday[key]}</p>
+              </div>
+          </div>`
             }
             container_push.innerHTML = final;
           } else if (input_value.includes("friday")) {
             let final = "";
             console.log(myObj.time_table.friday["11.00AM-12.00AM"]);
             for (let key in myObj.time_table.friday) {
-              let container_temp = document.createElement("div");
-              container_temp = "";
-              container_temp += `<h1 style="color:"red">${key}
-              <h2 style="color:blue">${myObj.time_table.friday[key]}</h2><br>`;
-              console.log(container_temp);
-              final += container_temp;
+              final += `<div class="card animate__pulse">
+              <div class="card-body">
+                  <h5 class="card-title">${key}</h5>
+                  <p class="card-text">${myObj.time_table.friday[key]}</p>
+              </div>
+          </div>`
             }
             container_push.innerHTML = final;
           } else {
